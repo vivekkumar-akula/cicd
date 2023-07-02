ab48dc660f631d33560e064173154c82d57d0e81
AKULA VIVEK KUMAR <76099244+vivekkumar-akula@users.noreply.github.com>
2023-07-03 04:59:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const sanitizer = require('sanitizer');
-
 const app = express();
 const port = 8000;
+const path = require('path');
+
+// Set the views folder
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride(function (req, res) {
@@ -17,44 +20,12 @@ app.use(methodOverride(function (req, res) {
 
 let projectList = [];
 
+// Routes
 app.get('/project', function (req, res) {
-  res.render('project.ejs', { projectList, project: null });
+  res.render('project.ejs', { projectList });
 });
 
-app.post('/project/add/', function (req, res) {
-  let newProject = sanitizer.escape(req.body.newProject);
-  if (req.body.newProject !== '') {
-    projectList.push(newProject);
-  }
-  res.redirect('/project');
-});
-
-app.get('/project/delete/:id', function (req, res) {
-  if (req.params.id !== '') {
-    projectList.splice(req.params.id, 1);
-  }
-  res.redirect('/project');
-});
-
-app.get('/project/:id', function (req, res) {
-  let projectIdx = req.params.id;
-  let project = projectList[projectIdx];
-
-  if (project) {
-    res.render('edit.ejs', { projectList, projectIdx, project });
-  } else {
-    res.redirect('/project');
-  }
-});
-
-app.put('/project/edit/:id', function (req, res) {
-  let projectIdx = req.params.id;
-  let editProject = sanitizer.escape(req.body.editProject);
-  if (projectIdx !== '' && editProject !== '') {
-    projectList[projectIdx] = editProject;
-  }
-  res.redirect('/project');
-});
+// ... rest of the routes ...
 
 app.use(function (req, res, next) {
   res.redirect('/project');
